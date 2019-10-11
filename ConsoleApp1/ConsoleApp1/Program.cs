ae61b1607fd940c8713f63408722d782fc096726
deartist22 <deartist22@gmail.com>
2019-10-11 17:23:22 -0500

    Remove line "Your number is {0}". Proof read and change all WriteLine declarations. Change the upper bound to 11 so it will include 10. Add Comments.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,18 +19,20 @@ namespace ConsoleApp1
         static void GuessingGame() {
 
             int number;
-            Random rand = new Random();
-            int randNum = rand.Next(1, 10);
-            Console.WriteLine("Guess the number!!!");
+            Random rand = new Random(); //Create Random number
+            int randNum = rand.Next(1, 11); //Random numbers from 1 to 10
+            Console.WriteLine("Guess what number I'm thinking!!!");
             number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your number is {0}", number);
-            
-            while(number != randNum){
-                Console.WriteLine("Incorrect the computer select {0}, guess again", randNum);
-                randNum = rand.Next(1, 10);
+       
+            while(number != randNum){ //Will run until both the machine and the user number are equal
+                Console.WriteLine("Incorrect, I was thinking of {0}, guess again", randNum);
+                randNum = rand.Next(1, 11);
                 number = Convert.ToInt32(Console.ReadLine());
+                if(number <= 0 || number > 10){ //Will check if number selected by user is between 1 to 10
+                    Console.WriteLine("Nope, I'm thinking between 1 and 10");
+                }
             }
-            Console.WriteLine("Equal {0} and  {1}, good guess", randNum, number);
+            Console.WriteLine("Yes, I was thinking of {0}, \"WINNER\"", randNum);
         
         }
     }
